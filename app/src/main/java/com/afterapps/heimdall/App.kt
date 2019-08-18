a99b0ddd9fc7ea839527f8a29fe48bb8205b539d
mahmoud <mahmoudalyudeen@gmail.com>
2019-08-18 21:44:09 +0200

    - implemented dependency injection

---
+++ app/src/main/java/com/afterapps/heimdall/App.kt
@@ -0,0 +1,19 @@
+package com.afterapps.heimdall
+
+import android.app.Application
+import com.afterapps.heimdall.di.collectionsModule
+import com.afterapps.heimdall.di.dataModule
+import com.afterapps.heimdall.di.imagesModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+@Suppress("unused")
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(listOf(dataModule, collectionsModule, imagesModule))
+        }
+    }
+}
---
+++ app/src/main/java/com/afterapps/heimdall/di/Modules.kt
@@ -0,0 +1,43 @@
+package com.afterapps.heimdall.di
+
+import androidx.room.Room
+import com.afterapps.heimdall.database.ShutterstockDatabase
+import com.afterapps.heimdall.network.ShutterstockApi
+import com.afterapps.heimdall.repository.CollectionsRepository
+import com.afterapps.heimdall.repository.ImagesRepository
+import com.afterapps.heimdall.ui.collections.CollectionsViewModel
+import com.afterapps.heimdall.ui.images.ImagesViewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+/** API and Database dependencies */
+val dataModule = module {
+
+    single { ShutterstockApi() }
+
+    single {
+        Room.databaseBuilder(get(), ShutterstockDatabase::class.java, "shutterstockdatabase").build()
+    }
+}
+
+/** ViewModel and Repository dependencies for the Collections view */
+val collectionsModule = module {
+
+    single { CollectionsRepository(shutterstockDatabase = get(), shutterstockApi = get()) }
+
+    viewModel { CollectionsViewModel(collectionsRepository = get()) }
+
+}
+
+/** ViewModel and Repository dependencies for the Images view */
+val imagesModule = module {
+
+    single {
+        ImagesRepository(shutterstockDatabase = get(), shutterstockApi = get())
+    }
+
+    viewModel { (collectionId: String) ->
+        ImagesViewModel(imagesRepository = get(), collectionId = collectionId)
+    }
+
+}
