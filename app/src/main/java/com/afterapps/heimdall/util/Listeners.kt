8e6e0392acb740946dbbd2263380eeb25a7cf89a
mahmoud <mahmoudalyudeen@gmail.com>
2019-08-23 20:34:24 +0200

    - refactored listeners out of util file

---
+++ app/src/main/java/com/afterapps/heimdall/util/Listeners.kt
@@ -0,0 +1,53 @@
+package com.afterapps.heimdall.util
+
+import android.graphics.drawable.Drawable
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+/** Listener for SearchView onQueryTextSubmit */
+class OnQueryTextListener(private val onTextSubmit: (query: String?) -> Unit) :
+    SearchView.OnQueryTextListener {
+    override fun onQueryTextSubmit(query: String?) = run {
+        onTextSubmit(query)
+        false
+    }
+
+    override fun onQueryTextChange(newText: String?) = false
+}
+
+/** Listener to respond to SearchView onMenuItemActionCollapse */
+class OnActionExpandListener(private val onCollapse: () -> Unit) : MenuItem.OnActionExpandListener {
+    override fun onMenuItemActionCollapse(item: MenuItem?) = run {
+        onCollapse()
+        false
+    }
+
+    override fun onMenuItemActionExpand(item: MenuItem?) = true
+}
+
+/** Glide DrawableRequestListener, executes onDone() on both success and failure */
+class DrawableRequestListener(private val onDone: () -> Unit) :
+    RequestListener<Drawable> {
+    override fun onResourceReady(r: Drawable?, a: Any?, t: Target<Drawable>?, d: DataSource?, i: Boolean) = run {
+        onDone()
+        false
+    }
+
+    override fun onLoadFailed(e: GlideException?, m: Any?, t: Target<Drawable>?, i: Boolean) = run {
+        onDone()
+        false
+    }
+}
+
+/** RecyclerView OnScrollListener with onScrollListener */
+class OnScrollListener(private val onScroll: () -> Unit) : RecyclerView.OnScrollListener() {
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        if (dy > 0) onScroll()
+        super.onScrolled(recyclerView, dx, dy)
+    }
+}
--- app/src/main/java/com/afterapps/heimdall/util/Util.kt
+++ app/src/main/java/com/afterapps/heimdall/util/Util.kt
@@ -2,16 +2,8 @@ package com.afterapps.heimdall.util
 
 import android.app.Activity
 import android.content.Context
-import android.graphics.drawable.Drawable
-import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
-import androidx.appcompat.widget.SearchView
-import androidx.recyclerview.widget.RecyclerView
 import com.afterapps.heimdall.network.CallStatus
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 
 /** Returns formatted URL after replacing placeholder with ID */
 fun getImageUrl(imageUrlFormat: String, imageId: String): String {
@@ -28,45 +20,5 @@ fun hideKeyboard(activity: Activity?) {
     val focusedView = activity.currentFocus
     if (focusedView != null) {
         inputManager.hideSoftInputFromWindow(focusedView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
-    }
-}
-
-/** Glide DrawableRequestListener, executes onDone() on both success and failure */
-class DrawableRequestListener(private val onDone: () -> Unit) : RequestListener<Drawable> {
-    override fun onResourceReady(r: Drawable?, a: Any?, t: Target<Drawable>?, d: DataSource?, i: Boolean) = run {
-        onDone()
-        false
-    }
-
-    override fun onLoadFailed(e: GlideException?, m: Any?, t: Target<Drawable>?, i: Boolean) = run {
-        onDone()
-        false
-    }
-}
-
-/** Listener for SearchView onQueryTextSubmit */
-class OnQueryTextListener(private val onTextSubmit: (query: String?) -> Unit) : SearchView.OnQueryTextListener {
-    override fun onQueryTextSubmit(query: String?) = run {
-        onTextSubmit(query)
-        false
-    }
-
-    override fun onQueryTextChange(newText: String?) = false
-}
-
-/** Listener to respond to SearchView onMenuItemActionCollapse */
-class OnActionExpandListener(private val onCollapse: () -> Unit) : MenuItem.OnActionExpandListener {
-    override fun onMenuItemActionCollapse(item: MenuItem?) = run {
-        onCollapse()
-        false
-    }
-
-    override fun onMenuItemActionExpand(item: MenuItem?) = true
-}
-
-class OnScrollListener(private val onScroll: () -> Unit) : RecyclerView.OnScrollListener() {
-    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-        if (dy > 0) onScroll()
-        super.onScrolled(recyclerView, dx, dy)
     }
 }
